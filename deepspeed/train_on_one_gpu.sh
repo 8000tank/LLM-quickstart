240fe42dd94b03f0277f68bba3a4688131426133
8000tank <8000tank@gmail.com>
2024-11-12 23:47:18 +0800

    十五章 DeepSpedd（第七次作业）

--- deepspeed/train_on_one_gpu.sh
+++ deepspeed/train_on_one_gpu.sh
@@ -2,7 +2,7 @@
 deepspeed --num_gpus=1 translation/run_translation.py \
 --deepspeed config/ds_config_zero2.json \
 --model_name_or_path t5-small --per_device_train_batch_size 1 \
---output_dir output_dir --overwrite_output_dir --fp16 \
+--output_dir /ssdData/llm/deepspeed/models/t5-small --overwrite_output_dir --fp16 \
 --do_train --max_train_samples 500 --num_train_epochs 1 \
 --dataset_name wmt16 --dataset_config "ro-en" \
 --source_lang en --target_lang ro
@@ -13,7 +13,7 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 --model_name_or_path t5-large \
 --per_device_train_batch_size 4 \
 --per_device_eval_batch_size 4 \
---output_dir output_dir --overwrite_output_dir \
+--output_dir /ssdData/llm/deepspeed/models/t5-large --overwrite_output_dir \
 --do_train \
 --do_eval \
 --max_train_samples 500 --num_train_epochs 1 \
@@ -22,11 +22,11 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 
 
 
-# DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
+# DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-3B）
 deepspeed --num_gpus=1 translation/run_translation.py \
 --deepspeed config/ds_config_zero3.json \
---model_name_or_path t5-3b --per_device_train_batch_size 1 \
---output_dir output_dir --overwrite_output_dir --fp16 \
+--model_name_or_path t5-3b --per_device_train_batch_size 1 --gradient_accumulation_steps 8 \
+--output_dir /ssdData/llm/deepspeed/models/t5-large --overwrite_output_dir --fp16 \
 --do_train --max_train_samples 500 --num_train_epochs 1 \
 --dataset_name wmt16 --dataset_config "ro-en" \
 --source_lang en --target_lang ro
@@ -43,7 +43,7 @@ python translation/run_translation.py \
     --source_prefix "translate English to Romanian: " \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
-    --output_dir tmp/tst-translation \
+    --output_dir /ssdData/llm/deepspeed/models/t5-small \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=4 \
     --overwrite_output_dir \
